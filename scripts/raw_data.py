8cdfa0c0892f87e385f63651c5c1f9e0d98d31fe
Dominic Mander <44836790+dominicmander@users.noreply.github.com>
2018-12-06 14:26:28 +0000

    refactor - separated db utils for reuse

--- scripts/raw_data.py
+++ scripts/raw_data.py
@@ -1,31 +1,8 @@
-from configparser import SafeConfigParser
 import os
 import pandas as pd
-from sqlalchemy import create_engine
 
 import data_types
-
-def db_config():
-    parser = SafeConfigParser()
-    parser.read('config.ini')
-    db = dict(parser.items("keyrus_snowflake_db"))
-    return db
-
-def db_engine():
-    engine = create_engine('snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}'.format(**db))
-    return engine
-
-def run_query(q):
-    with engine.connect() as conn:
-        return pd.read_sql(q, conn)
-
-def run_command(c):
-    with engine.connect() as conn:
-        conn.execute(c)
-
-def df_to_table(df, table, dtypes, exists='replace'):
-    with engine.connect() as conn:
-        df.to_sql(table, conn, dtype=dtypes, index=False, if_exists=exists, chunksize=10000)
+import db_utils
 
 def load_files(dir):
     for file in os.scandir(dir):
@@ -35,12 +12,10 @@ def load_files(dir):
             dtypes_csv = data_types.data_types_csv[name]
             data = pd.read_csv(path, dtype=dtypes_csv)
             dtypes_sql = data_types.data_types_sql[name]
-            df_to_table(data, name, dtypes_sql)
+            db_utils.df_to_table(data, name, dtypes_sql)
 
 if __name__ == '__main__':
-    db = db_config()
-    engine = db_engine()
-
+    db_utils.db_engine('raw_data_python')
     load_files('../data/')
     # first run end to end took 00:24:14 with X-SMALL warehouse
     
