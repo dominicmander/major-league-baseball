723fbf748e0e6f8f655fdef068b9802651679ae5
Dominic Mander <44836790+dominicmander@users.noreply.github.com>
2018-12-11 21:00:09 +0000

    add - logging

--- scripts/db_utils.py
+++ scripts/db_utils.py
@@ -1,6 +1,13 @@
+import logging
 from configparser import SafeConfigParser
 from sqlalchemy import create_engine
 import pandas as pd
+
+import setup_utils
+
+# prepare logging
+log = logging.getLogger(__name__)
+setup_utils.setup_logging(log)
 
 def db_engine(schema):
     """
@@ -12,7 +19,9 @@ def db_engine(schema):
     db['schema'] = schema
 
     global engine
+    log.info(f"creating snowflake engine: #{db['warehouse']}@{db['database']}.{schema}")
     engine = create_engine('snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}'.format(**db))
+    log.debug("engine created")
 
 def run_query(q):
     """
@@ -20,9 +29,13 @@ def run_query(q):
     """
     try:
         with engine.begin() as conn:
-            return pd.read_sql(q, conn)
+            q_log = ' '.join(q.split())[:50]
+            log.info(f"executing query: {q_log}")
+            result = pd.read_sql(q, conn)
+            log.debug("execution done")
+            return result
     except NameError:
-        print("no engine defined, use db_utils.db_engine(schema) to define")
+        log.warn("no engine defined, use db_utils.db_engine(schema) to define")
 
 def run_command(c):
     """
@@ -30,9 +43,12 @@ def run_command(c):
     """
     try:
         with engine.begin() as conn:
+            c_log = ' '.join(c.split())[:50]
+            log.info(f"executing command: {c_log}")
             conn.execute(c)
+            log.debug("execution done")
     except NameError:
-        print("no engine defined, use db_utils.db_engine(schema) to define")
+        log.warn("no engine defined, use db_utils.db_engine(schema) to define")
 
 def df_to_table(df, table, dtypes, exists='replace'):
     """
@@ -46,6 +62,8 @@ def df_to_table(df, table, dtypes, exists='replace'):
     """
     try:
         with engine.begin() as conn:
+            log.info(f"creating/replacing table: {table}")
             df.to_sql(table, conn, dtype=dtypes, index=False, if_exists=exists, chunksize=10000)
+            log.debug("execution done")
     except NameError:
-        print("no engine defined, use db_utils.db_engine(schema) to define")
+        log.warn("no engine defined, use db_utils.db_engine(schema) to define")
