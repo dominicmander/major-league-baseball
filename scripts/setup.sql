bf682ebb7dcf0ae48465bdb8850260dd9ae46511
Dominic Mander <44836790+dominicmander@users.noreply.github.com>
2018-12-28 19:12:30 +0000

    add - snowpipe schema and objects

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -12,6 +12,9 @@ create schema if not exists raw_data_python
 create schema if not exists raw_data_snowsql
     comment = 'raw data loaded in snowsql'
 ;
+create schema if not exists raw_data_snowpipe
+    comment = 'raw data loaded in snowpipe'
+;
 create schema if not exists mlb
     comment = 'core mlb data'
 ;
@@ -33,14 +36,34 @@ create or replace warehouse mlb_wh with
         type = csv
         skip_header = 1
         field_optionally_enclosed_by = '"'
+        comment = 'file format used by stage mlb_stage'
     ;
 
     --create stage
     create or replace stage mlb_stage
-        url = '&mlb_s3_bucket_url'
-        credentials = (aws_key_id='&mlb_aws_key_id' aws_secret_key='&mlb_aws_secret_key')
+        url = '&{mlb_s3_bucket_url}'
+        credentials = (aws_key_id='&{mlb_aws_key_id}' aws_secret_key='&{mlb_aws_secret_key}')
         file_format = mlb_csv
+        comment = 'bulk raw data stage'
     ;
 
     --list staged files
     list @mlb_stage;
+
+--prepare raw_data_snowpipe schema
+    use schema raw_data_snowpipe;
+
+    --create file format
+    create or replace file format mlb_txt
+        type = csv
+        field_optionally_enclosed_by = '"'
+        comment = 'file format used by stage mlb_snowpipe'
+    ;
+
+    --create stage
+    create or replace stage mlb_snowpipe
+        url = '&{mlb_s3_bucket_url}snowpipe-gamelog/'
+        credentials = (aws_key_id='&{mlb_aws_key_id}' aws_secret_key='&{mlb_aws_secret_key}')
+        file_format = mlb_txt
+        comment = 'snowpipe raw data stage'
+    ;
